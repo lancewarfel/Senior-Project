573c7131989c5b408fb72235a2cf7c8756e23fe0
Gavin Maynard <gmay8729@gmail.com>
2023-04-17 14:10:57 -0500

    note fixes

--- Socket Test/src/lib/Sort.svelte
+++ Socket Test/src/lib/Sort.svelte
@@ -4,31 +4,33 @@
     $: console.log($playerNameStore)
     $: allData = $gameDataStore
     $: allPlayers = $playerNameStore
+    
+    //$: storedPlayers = database.query("SELECT NAME FROM RLPLAYERS")
+    //$: userInputs = []?
 
     function wipeData(){
         gameDataStore.set([])
     }
 
-    // function submitData(){
+    // function submitData(){}
 
-        //loop through inputs?{
-            //if(input!=discard){
+        //loop through inputs?
 
-                //loop thorugh allData for all instances of allPlayers[i] (input index and allPlayer index should be same)
-                    //datbase.update("UPDATE RLPLAYERS SET (stat name here) = (stat name here) + (player stat) WHERE NAME = (input?))
+            //if(input!="discard")
 
-            //} else {
+                //loop thorugh allData 
+
+                    //if(allData[j].name == allPlayers[i])?? (input index and allPlayer index should be same)
+
+                        //datbase.update("UPDATE RLPLAYERS SET (stat name here) = (stat name here) + (player stat) WHERE NAME = (input?))
+
+            // else 
                 //do nothing i guess? 
-            //}
             
-        //}
-
-
     //     gameDataStore.set([]) //reset the gameDataStore
-    // }
+    //}
 
-    //$: storedPlayers = database.query("SELECT NAME FROM RLPLAYERS")
-    //$: userInputs = []?
+    
 </script>
 
 
@@ -57,7 +59,8 @@
 
 
 
-<!-- how to loop through all data to reach a player name -->
+<!-- this section shows how to loop through all data to reach a player name (in svelte, the idea can be transferred to js) -->
+
     <!-- {#each allData as data, i}
     {#if i > 0}
         <h1>Game {i}</h1>
